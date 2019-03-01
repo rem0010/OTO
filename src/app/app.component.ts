93d91783a1ff40757fc78d18e320249fd39d7b0e
HAYASHI TAKAO <rem@macbook-2.local>
2019-03-01 13:56:39 +0900

    Fix details

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,14 +3,11 @@ import { Platform } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import Auth0Cordova from '@auth0/cordova';
-import { TabsPage } from '../pages/tabs/tabs';
 
 @Component({
   templateUrl: 'app.html'
 })
 export class MyApp {
-  rootPage:any = TabsPage;
-
   // constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
   //   rootPage:any = HomePage;
 
