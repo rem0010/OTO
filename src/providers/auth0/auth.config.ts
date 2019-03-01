7ce0f4229c163d203980b8656f70631005009929
HAYASHI TAKAO <rem@macbook-2.local>
2019-03-01 11:17:56 +0900

    Add new plugin

---
+++ src/providers/auth0/auth.config.ts
@@ -0,0 +1,6 @@
+export const AUTH_CONFIG = {
+    clientID: 'YOUR_CLIENT_ID',// Auth0 に必要（大文字化：ID）
+    clientId: 'YOUR_CLIENT_ID', // Auth0Cordova に必要（大文字化：Id）
+    domain: 'YOUR_AUTH_DOMAIN',
+    packageIdentifier: 'your.app.id' // config.xml ウィジェット ID にあります（com.auth0.ionic など）
+  };
